5c9f38058284beb351f227b4ce843dbbd137c272
humberto <humberto.lizana@gmail.com>
2023-10-17 20:48:10 -0500

    correcion repository

--- src/main/java/com/lizana/microservicebankaccount/infrastructure/outpupport/BankAccountRepo.java
+++ src/main/java/com/lizana/microservicebankaccount/infrastructure/outpupport/BankAccountRepo.java
@@ -1,10 +1,11 @@
 package com.lizana.microservicebankaccount.infrastructure.outpupport;
 
+import com.lizana.microservicebankaccount.domain.entity.BankAccount;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BankAccountRepo extends ReactiveMongoRepository {
+public interface BankAccountRepo extends ReactiveMongoRepository<BankAccount,String> {
 }
