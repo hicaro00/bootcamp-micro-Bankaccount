cd756b00764ac95b7703f366d17f44ba87aa039c
humberto <humberto.lizana@gmail.com>
2023-10-27 09:57:21 -0500

    creacion de cuentas listo

--- src/main/java/com/lizana/microservicebankaccount/application/serviceimpl/BankAccountServiceImpl.java
+++ src/main/java/com/lizana/microservicebankaccount/application/serviceimpl/BankAccountServiceImpl.java
@@ -1,15 +1,14 @@
 package com.lizana.microservicebankaccount.application.serviceimpl;
 
 import com.lizana.microservicebankaccount.application.factory.BankAccountFactory;
-import com.lizana.microservicebankaccount.application.factory.impl.AhorroAccountFactory;
-import com.lizana.microservicebankaccount.application.factory.impl.CuentaCorrienteAccountFactory;
-import com.lizana.microservicebankaccount.application.factory.impl.PlazoFijoAccountFactory;
 import com.lizana.microservicebankaccount.application.utils.AccountNumberGenerator;
 import com.lizana.microservicebankaccount.domain.dtos.BankAccountDto;
 import com.lizana.microservicebankaccount.domain.utilsmaper.AccountUtil;
 import com.lizana.microservicebankaccount.infrastructure.inputport.BanckAccountService;
 import com.lizana.microservicebankaccount.infrastructure.outpupport.BankAccountRepo;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -17,9 +16,7 @@ import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Slf4j
@@ -62,33 +59,29 @@ public class BankAccountServiceImpl implements BanckAccountService {
 
   @Override
   public Mono<BankAccountDto> createdBanckAccount(BankAccountDto bankAccountDto) {
+
+
+    String customerId = bankAccountDto.getCustomerId();
     String accountType = bankAccountDto.getAccountType(); // Obtener el tipo de cuenta desde accountCreationRequest
-
-    Map<String, BankAccountFactory> factoryMap = new HashMap<>();
-    factoryMap.put("SAVINGS", new AhorroAccountFactory());
-    factoryMap.put("CURRENTACCOUNT", new CuentaCorrienteAccountFactory());
-    factoryMap.put("FIXEDTERM", new PlazoFijoAccountFactory());
-
-  //  BankAccountFactory selectedFactory = factoryMap.get(accountType);
-
-
+    BankAccountDto newAccount = new BankAccountDto();
 
     if ("SAVINGS".equals(accountType)) {
-    BankAccountDto newAccount = new BankAccountDto();
+    newAccount.setCustomerId(customerId);
     newAccount.setAccountType("SAVINGS");
     newAccount.setAccountNumber(AccountNumberGenerator.generateAccountNumber());
     newAccount.setMonthlyMovementLimit(20);
     newAccount.setMaintenanceFee(0.0);
     newAccount.setMaturityDate(null);
+    newAccount.setBalance(BigDecimal.ZERO);
     newAccount.setTransactionFee(0.01);
     newAccount.setOpeningDate(LocalDate.now());
     newAccount.setTemporaryBlock(true);
-    newAccount.setAccountStatus("normal");
+    newAccount.setAccountStatus("personal-vip");
     newAccount.setAvailableBalance(BigDecimal.ZERO);
     newAccount.setNumberTransactions(100);
     newAccount.setSignatory(new ArrayList<>(bankAccountDto.getSignatory()));
     } else if ("CURRENTACCOUNT".equals(accountType)) {
-      BankAccountDto newAccount = new BankAccountDto();
+      newAccount.setCustomerId(customerId);
       newAccount.setAccountType("CURRENTACCOUNT");
       newAccount.setMonthlyMovementLimit(20);
       newAccount.setAccountNumber(AccountNumberGenerator.generateAccountNumber());
@@ -98,36 +91,32 @@ public class BankAccountServiceImpl implements BanckAccountService {
       newAccount.setTransactionFee(0.01);
       newAccount.setOpeningDate(LocalDate.now());
       newAccount.setTemporaryBlock(false);
-      newAccount.setAccountStatus("normal"); //normal o pyme
+      newAccount.setAccountStatus("empresarial-pyme"); //normal o pyme
       newAccount.setAvailableBalance(BigDecimal.ZERO);
       newAccount.setSignatory(new ArrayList<>(bankAccountDto.getSignatory()));
-    }if ("FIXEDTERM".equals(accountType)){
-      BankAccountDto newAccount = new BankAccountDto();
+    }else if ("FIXEDTERM".equals(accountType)){
+      newAccount.setCustomerId(customerId);
       newAccount.setAccountType("FIXEDTERM");
       newAccount.setMonthlyMovementLimit(2);
       newAccount.setAccountNumber(AccountNumberGenerator.generateAccountNumber());
       newAccount.setMaintenanceFee(0);
       newAccount.setMaturityDate(null);
+      newAccount.setBalance(BigDecimal.ZERO);
       newAccount.setTransactionFee(0.01);
       newAccount.setOpeningDate(LocalDate.now());
       newAccount.setTemporaryBlock(false);
-      newAccount.setAccountStatus("normal");
+      newAccount.setAccountStatus("personal");
       newAccount.setAvailableBalance(BigDecimal.ZERO);
       newAccount.setNumberTransactions(2);
       newAccount.setSignatory(new ArrayList<>(bankAccountDto.getSignatory()));
     }  else {
-    System.out.println("Tipo de cuenta no soportado: " + accountType);
-    return Mono.error(new IllegalArgumentException("Tipo de cuenta no soportado: " + accountType));
+      Logger logger = LoggerFactory.getLogger(getClass());
+      logger.error("Tipo de cuenta no soportado: {}", accountType);
+      return Mono.error(new IllegalArgumentException("Tipo de cuenta no soportado: " + accountType));
     }
 
-
-    // Realiza las operaciones necesarias y devuelve un Mono<AccountCreationRequest> en lugar de Mono<BankAccountDto>
-      return bankAccountRepo.insert(AccountUtil.dtoToEntity(newAccounte))
-              .map(AccountUtil::entityToDto)
-              .map(dto -> {
-                // Puedes realizar las transformaciones necesarias para actualizar el objeto AccountCreationRequest
-                return bankAccountDto;
-              });
+    return bankAccountRepo.insert(AccountUtil.dtoToEntity(newAccount))
+            .map(AccountUtil::entityToDto);
 
   }
 
